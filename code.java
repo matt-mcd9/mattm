7733de40f0ced59dc19383f0db677764e56e60e1
matt-mcd9 <Mcdermott.m92@gmail.com>
2025-02-03 14:40:40 -0500

    Add files via upload

---
+++ code.java
@@ -0,0 +1,12 @@
+int c = 5 / b //fixed divide by zero exception
+<<<<<<< HEAD
+d= math.cos(math.pi/3);
+=======
+
+
+<<<<<<< HEAD
+int d = sqrt(pow(integral(theta, infinity)));
+>>>>>>> ce69d5d (added advanced expression)
+=======
+int d = sqrt(pow(integral(theta, 0)));
+>>>>>>> ddcba03 (fixed a bug in new test code)
