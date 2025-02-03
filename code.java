e7e3ecded42a714be58577c11b16f50857cbda00
Matt <mcdermott.m92@gmail.com>
2025-02-03 14:32:54 -0500

    initial revision

--- code.java
+++ code.java
@@ -1,2 +1,8 @@
 int c = 5 / b //fixed divide by zero exception
+<<<<<<< HEAD
 d= math.cos(math.pi/3);
+=======
+
+
+int d = sqrt(pow(integral(theta, infinity)));
+>>>>>>> ce69d5d (added advanced expression)
