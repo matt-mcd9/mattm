02f1ed90d0daee54bb2a7fe4f660ce04b0fe89fa
Matt <mcdermott.m92@gmail.com>
2025-02-03 14:35:39 -0500

    Initial commit

--- code.java
+++ code.java
@@ -4,5 +4,9 @@ d= math.cos(math.pi/3);
 =======
 
 
+<<<<<<< HEAD
 int d = sqrt(pow(integral(theta, infinity)));
 >>>>>>> ce69d5d (added advanced expression)
+=======
+int d = sqrt(pow(integral(theta, 0)));
+>>>>>>> ddcba03 (fixed a bug in new test code)
